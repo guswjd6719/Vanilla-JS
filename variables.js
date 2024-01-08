3aad33134a9c206cc159c57b66a724a254e1fb6b
hj <hj@testui-MacBookAir.local>
2024-01-08 22:03:27 +0900

    Js 기본 문법

---
+++ variables.js
@@ -0,0 +1,17 @@
+// const : 변경되지 않는 변수
+// let : 변경가능한 값
+
+const a = 6;
+const b = 2;
+let myName = "niko";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+console.log("my name is " + myName);
+
+myName = "nicolas";
+
+console.log("new name is " + myName);
+
